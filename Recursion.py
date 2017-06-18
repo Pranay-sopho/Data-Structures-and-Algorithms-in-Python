515e4ebc79b73079490e530458cf96765ecf1e91
Pranay Kotkar <kotkar.pranay04@gmail.com>
2017-06-17 23:32:13 -0700

    Code until chapter 7 Updated

--- Recursion.py
+++ Recursion.py
@@ -1,3 +1,6 @@
+import os
+
+
 def factorial(n):
     if n == 0:
         return 1
@@ -32,6 +35,7 @@ def draw_ruler(num_inches, major_length):
         draw_interval(major_length - 1)
         draw_line(major_length, str(j + 1))
 
+
 # draw_ruler(3, 6)
 
 
@@ -47,6 +51,7 @@ def binary_search(data, target, low, high):
         else:
             return binary_search(data, target, low, mid - 1)
 
+
 """
 data = [1,2,3,4,5,6,7]
 print(binary_search(data, 0, 0, 6))
@@ -59,3 +64,36 @@ def reverse(S, start, stop):
     else:
         S[start], S[stop] = S[stop], S[start]
         reverse(S, start + 1, stop - 1)
+
+
+def disk_usage(path):
+    total = os.path.getsize(path)
+    if os.path.isdir(path):
+        for child in os.listdir(path):
+            child_path = os.path.join(path, child)
+            total += disk_usage(child_path)
+    print('{0:<7}'.format(total), path)
+    return total
+
+
+# disk_usage('E:\BOOKS')
+
+def good_power(x, n):
+    if n == 0:
+        return 1
+    else:
+        partial = good_power(x, n // 2)
+        result = partial * partial
+        if n % 2 == 1:
+            result *= x
+        return result
+
+
+def binary_sum(S, start, stop):
+    if start >= stop:
+        return 0
+    elif start == stop - 1:
+        return S[start]
+    else:
+        mid = (stop + start) // 2
+        return binary_sum(S, start, mid) + binary_sum(S, mid, stop)
--- main.py
+++
@@ -1,7 +0,0 @@
-def reverse(S, start, stop):
-    """Reverse elements in implicit size"""
-    if start >= stop:
-        return 0
-    else:
-        S[start], S[stop] = S[stop], S[start]
-        reverse(S, start + 1, stop - 1)
